b064be173481602915ecb755e4b17d9c996de098
MP Dev <mnprasetya@gmail.com>
2023-06-27 13:29:48 +0700

    Update alyacv2.py

--- alyacv2.py
+++ alyacv2.py
@@ -4,7 +4,6 @@ import platform
 import socket
 import colorama
 from colorama import Fore
-import msvcrt
 
 class AlyacChecker:
     def __init__(self, hmi_list):
@@ -16,14 +15,7 @@ class AlyacChecker:
         return r"\\{}\\c$\ProgramData\ESTsoft\ALYac\log".format(ip)
 
     def get_directory_scan_path(self, ip):
-        architecture = platform.architecture()[0]
         base_directory = "ALYac"
-        if architecture == "32bit":
-            base_directory = "ALYac"
-        elif architecture == "64bit":
-            base_directory = "ALYac"
-        else:
-            raise ValueError("Unsupported architecture.")
         return r"\\{}\\c$\ProgramData\ESTsoft\{}\update".format(ip, base_directory)
 
     def get_latest_modified_date(self, folder_path, folder_name):
@@ -35,30 +27,33 @@ class AlyacChecker:
         else:
             return None
 
-    def run(self):
+    def parse_hmi_file(self):
         with open(self.hmi_list, "r") as file:
-            lines = file.read().strip().split('\n')
+            lines = [line.strip() for line in file if line.strip()]
 
-        # Group IPs by room
         rooms = {}
         current_room = None
+
         for line in lines:
-            line = line.strip()
-            if line:
-                if line.islower():
-                    current_room = line.capitalize()
-                    rooms[current_room] = []
-                else:
-                    rooms[current_room].append(line)
+            if line.islower():
+                current_room = line.capitalize()
+                rooms[current_room] = []
+            else:
+                ip, computer_name = line.split("|")
+                rooms[current_room].append((ip.strip(), computer_name.strip()))
+
+        return rooms
+
+    def run(self):
+        rooms = self.parse_hmi_file()
 
         separator = "⊱" * 1
         print(f"{Fore.CYAN}Room - IP - Hostname                       |  Alyac Status")
         print(f"⊱{Fore.RESET}")
+
         for room, ip_list in rooms.items():
             print(f"{Fore.CYAN}{room}:{Fore.RESET}")
-            counter = 0  # Reset the counter for each room
-            for ip in ip_list:
-                counter += 1
+            for counter, (ip, computer_name) in enumerate(ip_list, start=1):
                 folder_path = self.get_folder_path(ip)
                 server_scan_folder_name = "server_scan"
                 directory_scan_path = self.get_directory_scan_path(ip)
@@ -69,18 +64,19 @@ class AlyacChecker:
                         continue
 
                     modified_date_scan = self.get_latest_modified_date(folder_path, server_scan_folder_name)
-                    print(f"{counter}. {ip:<17}- {self.hostname:<20} |  {Fore.GREEN}Last Scan   {Fore.RESET} {modified_date_scan.strftime('%H:%M %Y-%m-%d'):<17}")
+                    modified_date_scan_str = modified_date_scan.strftime('%H:%M:%S %Y-%m-%d') if modified_date_scan else ""
+                    print(f"{counter}. {ip:<17}- {computer_name:<20} |  {Fore.GREEN}Last Scan   {Fore.RESET} {modified_date_scan_str:<17}")
 
                     timestamp = os.path.getmtime(directory_scan_path)
                     latest_update_date = datetime.datetime.fromtimestamp(timestamp)
-                    print(f"   {ip:<17}- {self.hostname:<20} |  {Fore.YELLOW}Last Update {Fore.RESET} {latest_update_date.strftime('%H:%M %Y-%m-%d'):<17}")
+                    print(f"   {ip:<17}- {computer_name:<20} |  {Fore.YELLOW}Last Update {Fore.RESET} {latest_update_date.strftime('%H:%M:%S %Y-%m-%d'):<17}")
 
                 except Exception as e:
-                    print(f"{counter}. {Fore.RED}{ip:<17} | {self.hostname:<20} | -                  | An error occurred: {str(e)}{Fore.RESET}")
+                    print(f"{counter}. {Fore.RED}{ip:<17} | {computer_name:<20} | -                  | An error occurred: {str(e)}{Fore.RESET}")
 
             print(f"{Fore.CYAN}{separator}{Fore.RESET}")
 
-        print(f"Result Checking        : {Fore.YELLOW}{self.current_time.strftime('%Y-%m-%d %H:%M:%S')}{Fore.RESET}")
+        print(f"Checking Time    : {Fore.YELLOW}{self.current_time.strftime('%H:%M:%S %Y-%m-%d')}{Fore.RESET}")
 
         self.wait_for_key()
 
